445da593c7ccbebdcfe4b02e3580630a95f27f36
Rico Putra Pradana <ricopradanaputra2198@gmail.com>
2025-12-27 14:30:52 +0700

    feat: add Nuxt minimal starter scaffold

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+</script>
+
+<template>
+    <div>
+        <NuxtRouteAnnouncer />
+        <NuxtWelcome />
+        <button @click="count++">Count is: {{ count }}</button>
+    </div>
+</template>
